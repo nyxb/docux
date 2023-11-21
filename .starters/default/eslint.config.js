89cf56a57388c20dd46c31154991e91c39ef33ba
nyxb <contact@nyxb.email>
2023-11-21 21:45:27 +0100

    🧹 chore: remove eslint.config.js and "type": "module" from package.json files
    📦 packaging: update dependencies and remove duplicates in package.json files

--- .starters/default/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import nyxb from '@nyxb/eslint-config'
-
-export default nyxb({
-ignores: ["dist","**/dist/**","node_modules","**/node_modules/**",".output","**/.output/**",".nuxt","**/.nuxt/**"]
-})
